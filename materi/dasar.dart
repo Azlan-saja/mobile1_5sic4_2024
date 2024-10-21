1ecb99a59e8a9b91b897d894836f6e4b98cdfe33
Azlan-saja <azlansaja19@gmail.com>
2024-10-21 19:18:58 +0700

    first

---
+++ materi/dasar.dart
@@ -0,0 +1,27 @@
+void main() {
+  // Print String
+  print("Hello Word!");
+  print("Ayam makan nasi. Minum susu.");
+  print("----------------");
+
+  // Print Variabel
+  var username = "azlan";
+  print(username);
+  print("----------------");
+
+  // Print 1 atau lebih variabel dan String
+  var nama = "Kudut";
+  var kota = "Medan";
+  print("Nama saya $nama dan tinggal di $kota.");
+  print("----------------");
+
+  // Print Kalkulator Sederhana
+  int num1 = 8;
+  int num2 = 2;
+  int hasil_tambah = num1 + num2;
+  print("Kalkulator Tambah => $num1 + $num2 = $hasil_tambah");
+  int hasil_kurang = num1 - num2;
+  print("Kalkulator Kurang => $num1 - $num2 = $hasil_kurang");
+  double hasil_bagi = num1 / num2;
+  print("Kalkulator Kurang => $num1 - $num2 = $hasil_bagi");
+}
